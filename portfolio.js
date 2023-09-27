4694fd3929ba0ce11f93c75607b1ebea813a781a
h-harshit <harshit.ju26@gmail.com>
2023-09-27 23:44:46 +0530

    Javascript DOM manipulation introduction

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,13 @@
 function sayHello() {
+  // DOM manipulation
   let userName = document.getElementById("username").value;
-  alert(`Hi ${userName}! Welcome to my portfolio`);
+  // alert(`Hi ${userName}! Welcome to my portfolio`);
+
+  let helloMsgDiv = document.getElementById("hello-message-container");
+  helloMsgDiv.innerText = `Hi ${userName}! Welcome to my portfolio`;
+  // helloMsgDiv.innerHTML = "<span class='green-text'>Hey There</span>";
+
+  let headerElement = document.getElementsByClassName("name-header")[0];
+  headerElement.innerText = userName;
+  headerElement.style.color = "red";
 }
